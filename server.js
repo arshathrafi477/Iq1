c7b4bb541f45bd6def77bae3afa99f1d2761afac
arshathrafi477 <arshathrafi477@gmail.com>
2026-06-18 11:27:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const MAX_ATTEMPTS = 5;
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || "smtp.gmail.com",
   port: parseInt(process.env.EMAIL_PORT) || 587,
-  secure: process.env.EMAIL_SECURE === "true",
+  secure: process.env.EMAIL_SECURE === "false",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
