08696f52325ad8f3a21129df6c8a28f70cb985e4
arshathrafi477 <arshathrafi477@gmail.com>
2026-06-18 10:55:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const otpRoutes = require("./routes/otpRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // Middleware
 app.use(express.json());
